dc598ca7bdc69323d26d101f23083c2336929ba7
larryvsdata <larryvsdata@gmail.com>
2019-05-29 17:35:59 -0700

    Forms Started

--- src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -1,24 +1,42 @@
 package academy.learnprogramming.controller;
 
 import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
+import academy.learnprogramming.service.TodoItemService;
 import academy.learnprogramming.util.Mappings;
 import academy.learnprogramming.util.ViewNames;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoItemController {
 
+    // ==fields ==
+    private final TodoItemService todoItemService;
+
+    // constructors
+
+
+    public TodoItemController(TodoItemService todoItemService) {
+        this.todoItemService = todoItemService;
+    }
+
     // == model attributes ==
     @ModelAttribute
     public TodoData todoData(){
-        return new TodoData();
+        return todoItemService.getData();
     }
 
     // http://localhost:8080/todo-list/items
     @GetMapping(Mappings.ITEMS)
     public String items(){
         return ViewNames.ITEMS_LIST;
+    }
+
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute TodoItem todoItem){
+        return "redirect:/"+Mappings.ITEMS;
     }
 }
--- src/main/java/academy/learnprogramming/service/ToRoItemServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package academy.learnprogramming.service;
-
-public class ToRoItemServiceImpl {
-}
